a5b287b2673499cc16ecea33c2e49453296554c3
Matt Nelson <mattnelson6992@gmail.com>
2022-06-14 14:50:06 -0500

    db test

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -5,8 +5,8 @@ const pool = new pg.Pool({
   database: "d3fgsipjtlodos",
   host: "ec2-52-204-195-41.compute-1.amazonaws.com",
   port: 5432,
-  max: 12,
-  idleTimeoutMillis: 30000,
+  user: "hyyejfoznudqye",
+  password: "0c748e5168dc0dde1b14167fa5f1160fc2a16b781b18e856e89956eb28b54c3a",
 });
 // exports
 module.exports = pool;
