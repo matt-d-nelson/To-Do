718656eef65fb4c58c8c41922c5372a635f24183
Matt Nelson <mattnelson6992@gmail.com>
2022-06-14 13:46:13 -0500

    heroku db test

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,12 @@
 // requires
-const pg = require('pg');
+const pg = require("pg");
 // pool
 const pool = new pg.Pool({
-    database: "d3fgsipjtlodos",
-    host: "ec2-52-204-195-41.compute-1.amazonaws.com",
-    connectionString: process.env.DATABASE_URL,
-    port: 5432,
-    max: 12,
-    idleTimeoutMillis: 30000
+  database: "d3fgsipjtlodos",
+  host: "ec2-52-204-195-41.compute-1.amazonaws.com",
+  port: 5432,
+  max: 12,
+  idleTimeoutMillis: 30000,
 });
 // exports
 module.exports = pool;
